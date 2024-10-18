85348900ee5c6017ef5201bcb7fe3c90fb8dde52
ivan-garcia-parras <ivangp@gmail.com>
2024-10-18 15:19:34 +0200

    Remove parameters and output logs

--- AzureDevOps/start.sh
+++ AzureDevOps/start.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
-export AZURE_DEVOPS_EXT_PAT=$1
-az pipelines variable list --organization $2 --project $3 --pipeline-id $4 > variables.json
+if [ -z "$1" ]
+then
+  export DEVOPS_DOCKER_REGISTRY_IMAGE_TAG=main
+else
+  export DEVOPS_DOCKER_REGISTRY_IMAGE_TAG=$1
+fi
+
+export AZURE_DEVOPS_EXT_PAT=$SYSTEM_ACCESSTOKEN
+az pipelines variable list --organization $SYSTEM_COLLECTIONURI --project $SYSTEM_TEAMPROJECTID --pipeline-id $SYSTEM_DEFINITIONID > variables.json
 
 while read -r variable
 do
-if [ $variable != "DEVOPS_DOCKER_REGISTRY_URL" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_IMAGE_NAME" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_IMAGE_TAG" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_USER_NAME" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_PASSWORD" ]
-then
+  if [ $variable != "DEVOPS_DOCKER_REGISTRY_URL" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_IMAGE_NAME" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_IMAGE_TAG" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_USER_NAME" ] && [ $variable != "DEVOPS_DOCKER_REGISTRY_PASSWORD" ]
+  then
     env | grep $(echo $variable) >> env.list
-fi
+  fi
 done < <(jq -r 'keys[]' variables.json)
 
 env | grep 'SYSTEM_ACCESSTOKEN=' >> env.list | exit 0
@@ -21,7 +28,5 @@ env | grep 'SYSTEM_PULLREQUEST_PULLREQUESTID=' >> env.list | exit 0
 env | grep 'SYSTEM_PULLREQUEST_SOURCEBRANCH=' >> env.list | exit 0
 env | grep 'SYSTEM_PULLREQUEST_TARGETBRANCH=' >> env.list | exit 0
 
-cat env.list
-
 docker login $DEVOPS_DOCKER_REGISTRY_URL --username "$DEVOPS_DOCKER_REGISTRY_USER_NAME" --password-stdin <<< $DEVOPS_DOCKER_REGISTRY_PASSWORD
-docker run --env-file env.list $DEVOPS_DOCKER_REGISTRY_URL/$DEVOPS_DOCKER_REGISTRY_IMAGE_NAME:$5
+docker run --quiet --env-file env.list $DEVOPS_DOCKER_REGISTRY_URL/$DEVOPS_DOCKER_REGISTRY_IMAGE_NAME:$DEVOPS_DOCKER_REGISTRY_IMAGE_TAG
