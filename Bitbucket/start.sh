e324d1d46815f636baf88f933ad77866fb564a77
ivan-garcia-parras <ivangp@gmail.com>
2024-11-10 14:55:58 +0100

    variables.json

--- Bitbucket/start.sh
+++ Bitbucket/start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "BITBUCKET_WORKSPACE"
-echo $BITBUCKET_WORKSPACE
-echo "BITBUCKET_REPO_SLUG"
-echo $BITBUCKET_REPO_SLUG
-curl --request GET --url 'https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables' --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json"
-env
+curl --proto "=https" --tlsv1.2 -sSf -L "https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables" --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json" > variables.json
+
+cat variables.json
