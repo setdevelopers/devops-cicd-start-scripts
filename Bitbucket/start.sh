c8510b6d3ebf5303350fb93d11db8812608f319a
ivan-garcia-parras <ivangp@gmail.com>
2024-11-10 14:30:01 +0100

    env

--- Bitbucket/start.sh
+++ Bitbucket/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 
+echo $BITBUCKET_WORKSPACE
+echo $BITBUCKET_REPO_SLUG
 curl --request GET --url 'https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables' --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json"
+env
