83b626d50c3553cdd0d413d48005befb97096964
ivan-garcia-parras <ivangp@gmail.com>
2024-11-10 15:10:38 +0100

    Add DEVOPS_DOCKER_REGISTRY_IMAGE_TAG

--- Bitbucket/start.sh
+++ Bitbucket/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]
+then
+  export DEVOPS_DOCKER_REGISTRY_IMAGE_TAG=main
+else
+  export DEVOPS_DOCKER_REGISTRY_IMAGE_TAG=$1
+fi
+
 curl --proto "=https" --tlsv1.2 -sSf -L "https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables" --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json" > variables.json
 
 while read -r key
