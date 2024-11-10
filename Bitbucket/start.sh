56f49a34bd679cb0633eaea5a131581df62c95a2
ivan-garcia-parras <ivangp@gmail.com>
2024-11-10 15:03:31 +0100

    Bitbucket get repository variables

--- Bitbucket/start.sh
+++ Bitbucket/start.sh
@@ -3,4 +3,12 @@ set -e
 
 curl --proto "=https" --tlsv1.2 -sSf -L "https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables" --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json" > variables.json
 
-cat variables.json
+while read -r key
+do
+  if [ $key != "DEVOPS_DOCKER_REGISTRY_URL" ] && [ $key != "DEVOPS_DOCKER_REGISTRY_IMAGE_NAME" ] && [ $key != "DEVOPS_DOCKER_REGISTRY_IMAGE_TAG" ] && [ $key != "DEVOPS_DOCKER_REGISTRY_USER_NAME" ] && [ $key != "DEVOPS_DOCKER_REGISTRY_PASSWORD" ]
+  then
+    env | grep $(echo "$key=") >> env.list | exit 0
+  fi
+done < <(jq -r '.values[].key' variables.json)
+
+cat env.list
