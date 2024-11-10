63c44b32c59defb8eec835f47975b4a7d0d618d4
ivan-garcia-parras <ivangp@gmail.com>
2024-11-10 14:28:48 +0100

    Remove {}

--- Bitbucket/start.sh
+++ Bitbucket/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl --request GET --url 'https://api.bitbucket.org/2.0/repositories/${BITBUCKET_WORKSPACE}/${BITBUCKET_REPO_SLUG}/pipelines_config/variables' --header "Authorization: Bearer ${BITBUCKET_TOKEN}" --header "Accept: application/json"
+curl --request GET --url 'https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACE/$BITBUCKET_REPO_SLUG/pipelines_config/variables' --header "Authorization: Bearer $BITBUCKET_TOKEN" --header "Accept: application/json"
